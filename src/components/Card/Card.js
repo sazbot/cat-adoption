57802b1e9b04c31c5192ac935f8fc9860c80bec1
Sarah <81199534+sazbot@users.noreply.github.com>
2022-10-18 17:13:11 +0100

    complete test script for Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,34 @@
-function Card() {
-  return <div></div>;
+import { useState } from "react";
+import heartFilled from "../../svgs/heartFilled.svg";
+import heartOutlined from "../../svgs/heartOutlined.svg";
+import "./Card.css";
+
+function Card({ name, phone, email, image, isFavourite }) {
+  const [isFavoured, setIsFavoured] = useState(isFavourite);
+
+  function toggleFavoured() {
+    setIsFavoured(!isFavoured);
+  }
+
+  return (
+    <div className="card">
+      <div className="card-header">
+        <img src={image.url} alt={image.alt} className="card-img" />
+        <button className="heart" onClick={toggleFavoured}>
+          {isFavoured ? (
+            <img src={heartFilled} alt="filled heart" />
+          ) : (
+            <img src={heartOutlined} alt="outlined heart" />
+          )}
+        </button>
+      </div>
+      <div className="card-content">
+        <h3>{name}</h3>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/Card/__test__/Card.test.js
+++ src/components/Card/__test__/Card.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
 import Card from "../Card";
 
 const cardProps = {
@@ -6,10 +7,10 @@ const cardProps = {
   phone: "111-111-1111",
   email: "laith@hotmail.com",
   image: {
-    url: "https://unsplash.com/photos/TCsnvDyfreQ?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
+    url: "https://images.unsplash.com/photo-1512200331909-44d741611b43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=878&q=80",
     alt: "cute cat",
   },
-  isFavoured: false,
+  isFavourite: false,
 };
 
 describe("Card", () => {
@@ -35,5 +36,30 @@ describe("Card", () => {
   test("should show image with correct source", () => {
     render(<Card {...cardProps} />);
     expect(screen.getByAltText(/cute cat/i).src).toBe(cardProps.image.url);
+  });
+
+  test("should show outlined heart", () => {
+    render(<Card {...cardProps} />);
+    expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+    expect(screen.getByAltText(/outlined heart/i)).toBeInTheDocument();
+  });
+
+  test("should show filled heart", () => {
+    render(<Card {...cardProps} isFavourite={true} />);
+    expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+    expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+  });
+
+  test("should toggle heart status", () => {
+    render(<Card {...cardProps} />);
+    userEvents.click(screen.getByRole("button"));
+
+    expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+    expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+
+    userEvents.click(screen.getByRole("button"));
+
+    expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+    expect(screen.getByAltText(/outlined heart/i)).toBeInTheDocument();
   });
 });
